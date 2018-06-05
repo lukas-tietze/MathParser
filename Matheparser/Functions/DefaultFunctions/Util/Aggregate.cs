ae36a33d87daa329454d119243a80af018885eae
Lukas Tietze <lukas.tietze@s2014.tu-chemnitz.de>
2018-06-05 19:54:53 +0200

    Aggreagte

--- Matheparser/Functions/DefaultFunctions/Util/Aggregate.cs
+++ Matheparser/Functions/DefaultFunctions/Util/Aggregate.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Matheparser.Exceptions;
+﻿using Matheparser.Exceptions;
+using Matheparser.Parsing;
+using Matheparser.Parsing.Evaluation;
+using Matheparser.Tokenizing;
 using Matheparser.Values;
+using Matheparser.Variables;
 
 namespace Matheparser.Functions.DefaultFunctions.Util
 {
@@ -20,7 +19,72 @@ namespace Matheparser.Functions.DefaultFunctions.Util
 
         public IValue Eval(IValue[] parameters)
         {
-            return null;
+            this.Validate(parameters);
+
+            var add = parameters[0].AsString == "+";
+            var count = (int)parameters[1].AsDouble;
+            var res = 0.0;
+            var evaluator = default(PostFixEvaluator);
+            var counter = new Variable(parameters[2].AsString, 0);
+            var config = ConfigBase.DefaultConfig;
+            VariableManager.Instance.Define(counter);
+
+            try
+            {
+                var tokenizer = new Tokenizer(parameters[3].AsString, config);
+                tokenizer.Run();
+                var parser = new Parser(tokenizer.Tokens, config);
+                evaluator = new PostFixEvaluator(parser.CreatePostFixExpression(), config);
+            }
+            catch (System.Exception e)
+            {
+                throw new OperandEvaluationException();
+            }
+
+            for (var i = 0; i < count; i++)
+            {
+                counter.Value = ValueCreator.Create(i);
+                var aggregate = evaluator.Run();
+
+                if (aggregate.Type != ValueType.Number)
+                {
+                    throw new OperandEvaluationException();
+                }
+
+                if (add)
+                {
+                    res += aggregate.AsDouble;
+                }
+                else
+                {
+                    res *= aggregate.AsDouble;
+                }
+            }
+
+            VariableManager.Instance.Remove(counter.Name);
+
+            return new DoubleValue(res);
+        }
+
+        private void Validate(IValue[] parameters)
+        {
+            if (parameters.Length != 4)
+            {
+                throw new OperandNumberException();
+            }
+
+            if (parameters[0].Type != ValueType.String ||
+                parameters[1].Type != ValueType.Number ||
+                parameters[2].Type != ValueType.String ||
+                parameters[3].Type != ValueType.String)
+            {
+                throw new WrongOperandTypeException();
+            }
+
+            if (!parameters[0].AsString.Equals("+") && !parameters[0].AsString.Equals("*"))
+            {
+                throw new OperandEvaluationException();
+            }
         }
     }
 }
