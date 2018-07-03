ab43d80cfa2156478d944fa7745e97f72f1c6d71
Lukas Tietze <lukas.tietze@ipml.de>
2018-07-03 10:02:56 +0200

    Debuginfo & string representation

--- Lib/Values/ExpressionValue.cs
+++ Lib/Values/ExpressionValue.cs
@@ -1,13 +1,16 @@
 ﻿namespace Matheparser.Values
 {
     using System.Collections.Generic;
+    using System.Diagnostics;
     using Matheparser.Functions;
     using Matheparser.Parsing.Evaluation;
     using Matheparser.Parsing.PostFixExpressions;
     using Matheparser.Util;
 
+    [DebuggerDisplay("Expression: {rawExpression}")]
     public sealed class ExpressionValue : IValue
     {
+        private readonly string rawExpression;
         private readonly CalculationContext context;
         private readonly IReadOnlyList<IPostFixExpression> expressions;
         private IValue lastResult;
@@ -15,7 +18,7 @@
         public ExpressionValue(CalculationContext context, string expression)
         {
             this.context = context;
-
+            this.rawExpression = expression;
             var tokenizer = new Tokenizing.Tokenizer(expression, this.context.Config);
             var parser = new Parsing.Parser(tokenizer.Tokens, this.context.Config);
             this.expressions = parser.CreatePostFixExpression();
@@ -76,6 +79,11 @@
         private void Eval()
         {
             this.lastResult = new PostFixEvaluator(this.expressions, this.context.Config).Run();
+        }
+
+        public override string ToString()
+        {
+            return this.rawExpression;
         }
     }
 }
